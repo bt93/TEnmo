5538cd2eb586ef55f731764856a5b92c47d03a80
Ricky Cerny <cerny.ricky@gmail.com>
2020-06-30 15:08:53 -0400

    Added a SQL script to create a few transfers

---
+++ 09-Capstone/database/AddTransfers.sql
@@ -0,0 +1,11 @@
+select * from transfer_types
+
+
+select * from  transfer_statuses
+
+select * from transfers 
+
+Insert transfers (transfer_type_id, transfer_status_id, account_from, account_to, amount)
+    Values (1, 2, 1, 2, 500.00)
+Insert transfers (transfer_type_id, transfer_status_id, account_from, account_to, amount)
+    Values (1, 2, 2, 1, 350.00)
